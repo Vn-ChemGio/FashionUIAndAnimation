294e7067252802b15cdd81e891646ee207286069
Vn-ChemGio <vn.chemgio@yahoo.com>
2020-12-21 22:26:47 +0700

    Start welcome screen

--- navigators/AuthenticationNavigator.tsx
+++ navigators/AuthenticationNavigator.tsx
@@ -8,7 +8,7 @@ const AuthenticationStack = createStackNavigator<Routes>();
 
 const AuthenticationNavigator = () => {
     return (
-        <AuthenticationStack.Navigator headerMode="none">
+        <AuthenticationStack.Navigator headerMode="none" initialRouteName="OnBoarding">
             <AuthenticationStack.Screen name="OnBoarding" component={OnBoarding}/>
             <AuthenticationStack.Screen name="Welcome" component={Welcome}/>
 
